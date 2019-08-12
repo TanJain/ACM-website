2452198e5624f9189fe5c6486145baac295254df
TanJain <tanmayj55@gmail.com>
2019-08-12 15:48:44 +0530

    Navbar fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function () {
     $(document).scroll(function () {
-      var $nav = $(".fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      var $na = $(".fixed-top");
+      $na.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
